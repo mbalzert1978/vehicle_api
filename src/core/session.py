8d3c2d6afb55667e20b2c6c452d6d908d9d43482
Markus Iorio <markus.iorio@ionos.com>
2023-09-13 15:46:27 +0200

    session abstraction

--- src/core/session.py
+++ src/core/session.py
@@ -1,10 +1,73 @@
 """Sqlalchemy related module."""
+from __future__ import annotations
+
+from collections.abc import Callable
+from typing import Protocol
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from src.core.config import settings
 from src.model.sql_alchemy import mapper_registry, vehicle_table
 from src.model.vehicle import Vehicle
+
+
+class Session(Protocol):
+
+    def __enter__(self) -> Session:
+        ...
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        ...
+
+    def execute(self, statement: str, *arg, **kwargs) -> None:
+        ...
+
+    def get(self, *args, **kwargs) -> None:
+        ...
+
+    def close(self, *args, **kwargs) -> None:
+        ...
+
+    def commit(self, *args, **kwargs) -> None:
+        ...
+
+    def refresh(self, *args, **kwargs) -> None:
+        ...
+
+    def add(self, *args, **kwargs) -> None:
+        ...
+
+
+class AbstractSessionMaker(Protocol):
+
+    def __call__(self) -> Session:
+        ...
+
+
+class SQLAlchemySessionMaker(AbstractSessionMaker):
+
+    def __init__(self,
+                 autocommit: bool = False,
+                 autoflush: bool = False,
+                 echo: bool = settings.ECHO,
+                 callback: Callable | None = None) -> None:
+        self.autocommit = autocommit
+        self.autoflush = autoflush
+        self.echo = echo
+        self.callback = callback or create_engine
+        self.engine = self.callback(
+            fetch_db_uri(),
+            pool_pre_ping=True,
+            echo=self.echo,
+        )
+        mapper_registry.map_imperatively(Vehicle, vehicle_table)
+
+    def __call__(self, client: Callable | None = None) -> Session:
+        session = client or sessionmaker(autocommit=self.autocommit,
+                                         autoflush=self.autoflush,
+                                         bind=self.engine)
+        return session()
 
 
 def fetch_db_uri() -> str:
@@ -26,12 +89,4 @@ def fetch_db_uri() -> str:
     return settings.DATABASE_URI
 
 
-engine = create_engine(
-    fetch_db_uri(),
-    pool_pre_ping=True,
-    echo=settings.ECHO,
-)
-mapper_registry.map_imperatively(Vehicle, vehicle_table)
-
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = SQLAlchemySessionMaker()
