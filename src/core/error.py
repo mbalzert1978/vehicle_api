6c2147327c71c3dab21360dad2e85f1b5618186f
Markus Iorio <mbalzert@gmail.com>
2023-10-31 22:49:12 +0100

    exceptions

--- src/core/error.py
+++ src/core/error.py
@@ -8,3 +8,12 @@ class HTTPError(Exception):
     def __init__(self, status_code: int, detail: str) -> None:
         self.status_code = status_code
         self.detail = detail
+
+
+class UnwrapError(Exception):
+    def __init__(self, from_: BaseException) -> None:
+        self.from_ = from_
+
+
+class ReadOnlyError(Exception):
+    """Error raised when trying to modify a read-only object."""
