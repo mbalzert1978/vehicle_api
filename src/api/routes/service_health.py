d632db7208db578df1309210cb9b7de02f579438
Markus Iorio <mbalzert@gmail.com>
2023-12-06 14:00:42 +0100

    correct status

--- src/api/routes/service_health.py
+++ src/api/routes/service_health.py
@@ -7,16 +7,17 @@ from fastapi import APIRouter, Depends
 
 from src import crud
 from src.api.dependecies.database import get_repository
+from src.schemas.status import DatabaseStatus
 
 router = APIRouter()
 
 log = logging.getLogger(__name__)
 
 
-@router.get("/")
+@router.get("/", response_model=DatabaseStatus)
 def database_status(
     repository: crud.AbstractRepository = Depends(get_repository(crud.SQLAlchemyRepository)),
-) -> dict[str, str]:
+)  -> DatabaseStatus:
     """Check the database status."""
     repository.execute(stmnt="SELECT 1=1;")
-    return {"status": "Ok"}
+    return DatabaseStatus()
--- tests/e2e/test_service_health.py
+++ tests/e2e/test_service_health.py
@@ -1,8 +1,9 @@
-from fastapi import status
 from fastapi.testclient import TestClient
+
+from src.schemas.status import DatabaseStatus
 
 
 def test_service_health_happy_path(client: TestClient):
     # Test service health
     result = client.get("/api/service")
-    assert result.status_code == status.HTTP_200_OK
+    assert result.json() == DatabaseStatus().model_dump()
