029006181e07a1edd73012793d2db5baea2b9827
Markus Iorio <mbalzert@gmail.com>
2023-12-06 10:30:17 +0100

    using const for text

--- src/schemas/vehicle.py
+++ src/schemas/vehicle.py
@@ -4,6 +4,11 @@ import json
 
 from pydantic import BaseModel, ConfigDict, Field, ValidationInfo, field_validator
 
+DESCIPTION_NAME = "The name of the vehicle."
+DESCRIPTION_YOM = "The year of manufacture for the vehicle."
+DESCRIPTION_RTD = "Whether the vehicle is ready to drive."
+DESCRIPTION_BODY = "Additional information about the vehicle in the form of a dictionary."
+
 
 def _get_year_now() -> int:
     return datetime.datetime.now(tz=datetime.UTC).year
@@ -13,28 +18,22 @@ class VehicleBase(BaseModel):
 
     """Base vehicle model."""
 
-    name: str = Field(description="The name of the vehicle.", examples=["Audi"])
+    name: str = Field(description=DESCIPTION_NAME, examples=["Audi"])
     year_of_manufacture: int = Field(
-            description="The year of manufacture for the vehicle.",
-            ge=2000,
-            le=_get_year_now(),
-            default=_get_year_now(),
-        )
-    ready_to_drive: bool = Field(
-            description="Whether the vehicle is ready to drive.",
-            default=False,
+        description=DESCRIPTION_YOM,
+        ge=2000,
+        le=_get_year_now(),
+        examples=[1999],
+        default=_get_year_now(),
     )
+    ready_to_drive: bool = Field(description=DESCRIPTION_RTD, default=False)
 
 
 class VehicleCreate(VehicleBase):
 
     """Vehicle create model."""
 
-    body: dict = Field(
-        description="Additional information about the vehicle in the form of a dictionary.",
-        default_factory=dict,
-        examples=[dict(color="black")],
-    )
+    body: dict = Field(description=DESCRIPTION_BODY, default_factory=dict, examples=[dict(color="black")])
 
     @field_validator("body", mode="before")
     @classmethod
@@ -58,26 +57,16 @@ class VehicleUpdate(BaseModel):
 
     """Vehicle update model."""
 
-    name: str | None = Field(
-        description="The name of the vehicle.",
-        examples=["Audi"],
-        default=None,
-    )
+    name: str | None = Field(description=DESCIPTION_NAME, examples=["Audi"], default=None)
     year_of_manufacture: int | None = Field(
-        description="The year of manufacture for the vehicle.",
+        description=DESCRIPTION_YOM,
         ge=1980,
         le=_get_year_now(),
+        examples=[1999],
         default=None,
     )
-    body: dict | None = Field(
-        description="Additional information about the vehicle in the form of a dictionary.",
-        default_factory=dict,
-        examples=[dict(color="black")],
-    )
-    ready_to_drive: bool = Field(
-        description="Whether the vehicle is ready to drive.",
-        default=None,
-    )
+    body: dict | None = Field(description=DESCRIPTION_BODY, default_factory=dict, examples=[dict(color="black")])
+    ready_to_drive: bool = Field(description=DESCRIPTION_RTD, default=None)
 
 
 class VehicleInDBBase(VehicleCreate):
