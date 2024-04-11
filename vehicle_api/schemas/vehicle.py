bc3b01b5294a4f8b0bb92a53540b2481a7b90009
Markus Iorio <mbalzert@gmail.com>
2024-04-11 13:33:26 +0200

    simplify

--- vehicle_api/schemas/vehicle.py
+++ vehicle_api/schemas/vehicle.py
@@ -10,30 +10,29 @@ DESCRIPTION_NAME = "The name of the vehicle."
 DESCRIPTION_YOM = "The year of manufacture for the vehicle."
 DESCRIPTION_RTD = "Whether the vehicle is ready to drive."
 DESCRIPTION_BODY = "Additional information about the vehicle in the form of a dictionary."
-NOW_YEAR = utc_now().year
 
-FIELD_NAME = functools.partial(Field, description=DESCRIPTION_NAME, examples=["Audi"])
-FIELD_YOM = functools.partial(Field, description=DESCRIPTION_YOM, le=NOW_YEAR, examples=[1999])
-FIELD_RTD = functools.partial(Field, description=DESCRIPTION_RTD)
-FIELD_BODY = functools.partial(Field, description=DESCRIPTION_BODY, examples=[dict(color="black")])
+field_name = functools.partial(Field, description=DESCRIPTION_NAME, examples=["Audi"])
+field_yom = functools.partial(Field, description=DESCRIPTION_YOM, le=utc_now().year, examples=[1999])
+field_rtd = functools.partial(Field, description=DESCRIPTION_RTD)
+field_body = functools.partial(Field, description=DESCRIPTION_BODY, examples=[dict(color="black")])
 
 
 class VehicleForCreate(BaseModel):
     """Vehicle create model."""
 
-    name: str = FIELD_NAME()
-    year_of_manufacture: int = FIELD_YOM(default=NOW_YEAR)
-    ready_to_drive: bool = FIELD_RTD(default=False)
-    body: dict = FIELD_BODY(default_factory=dict)
+    name: str = field_name()
+    year_of_manufacture: int = field_yom(default=utc_now().year)
+    ready_to_drive: bool = field_rtd(default=False)
+    body: dict = field_body(default_factory=dict)
 
 
 class VehicleForUpdate(BaseModel):
     """Vehicle update model."""
 
-    name: str | None = FIELD_NAME(default=None)
-    year_of_manufacture: int | None = FIELD_YOM(default=None)
-    ready_to_drive: bool | None = FIELD_RTD(default=None)
-    body: dict | None = FIELD_BODY(default=None)
+    name: str | None = field_name(default=None)
+    year_of_manufacture: int | None = field_yom(default=None)
+    ready_to_drive: bool | None = field_rtd(default=None)
+    body: dict | None = field_body(default=None)
 
 
 class VehicleFromDatabase(VehicleForCreate):
