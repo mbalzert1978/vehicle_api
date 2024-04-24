c2963f7f235ad15fe4b2202fb624636c677c7ac4
Markus Iorio <mbalzert@gmail.com>
2024-04-24 09:59:29 +0200

    using new config, uuidv7 in main file

--- src/main.py
+++ src/main.py
@@ -1,55 +1,61 @@
 """Vehicle api main module."""
 
 import logging
-from uuid import uuid4
 
-from asgi_correlation_id import CorrelationIdMiddleware
-from asgi_correlation_id.middleware import is_valid_uuid4
-from fastapi import FastAPI
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.middleware.cors import CORSMiddleware
+import asgi_correlation_id as cid
+import fastapi
+import starlette.middleware.base as sbase
+import starlette.middleware.cors as scors
+import uuid_utils as uuid
 
-from src.api.routes.api import router as api_router
-from src.core.config import get_app_settings
+from src.config import get_settings
 from src.core.logging import configure_logging
+from src.middlewares.error_handler import error_handling_middleware
+from src.middlewares.log import logging_middleware
 from src.middlewares.time import add_process_time_header
-
-from .middlewares.error_handler import error_handling_middleware
-from .middlewares.log import logging_middleware
+from src.vehicle.router import router as vehicle_router
 
 logger = logging.getLogger(__name__)
 
-settings = get_app_settings()
-configure_logging()
+
+def _is_valid_uuid7(uuid_: str) -> bool:
+    """
+    Check whether a string is a valid v4 uuid.
+    """
+    try:
+        return bool(uuid.UUID(uuid_, version=7))
+    except ValueError:
+        return False
 
 
-def get_application() -> FastAPI:
-    settings = get_app_settings()
+def get_application() -> fastapi.FastAPI:
+    settings = get_settings()
+    configure_logging()
 
-    application = FastAPI(**settings.fastapi_kwargs)
+    application = fastapi.FastAPI(**settings.fastapi_kwargs)
 
     application.add_middleware(
-        CORSMiddleware,
-        allow_origins=settings.allowed_hosts,
+        scors.CORSMiddleware,
+        allow_origins=settings.CORS_ORIGINS,
         allow_credentials=True,
         allow_methods=["*"],
-        allow_headers=["*"],
+        allow_headers=settings.CORS_HEADERS,
     )
 
-    application.add_middleware(BaseHTTPMiddleware, dispatch=add_process_time_header)
-    application.add_middleware(BaseHTTPMiddleware, dispatch=logging_middleware)
-    application.add_middleware(BaseHTTPMiddleware, dispatch=error_handling_middleware)
+    application.add_middleware(sbase.BaseHTTPMiddleware, dispatch=add_process_time_header)
+    application.add_middleware(sbase.BaseHTTPMiddleware, dispatch=logging_middleware)
+    application.add_middleware(sbase.BaseHTTPMiddleware, dispatch=error_handling_middleware)
 
     application.add_middleware(
-        CorrelationIdMiddleware,
+        cid.CorrelationIdMiddleware,
         header_name="X-Correlation-ID",
         update_request_header=True,
-        generator=lambda: uuid4().hex,
-        validator=is_valid_uuid4,
+        generator=lambda: uuid.uuid7().hex,
+        validator=_is_valid_uuid7,
         transformer=lambda a: a,
     )
 
-    application.include_router(api_router, prefix=settings.api_prefix)
+    application.include_router(vehicle_router, tags=vehicle_router.tags)
 
     return application
 
