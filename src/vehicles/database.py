0180a289cc8b6b0492f0dde812432e7ad7dd313f
Markus Iorio <mbalzert@gmail.com>
2024-04-24 13:54:18 +0200

    correct impl for sql alchemy

--- src/vehicles/database.py
+++ src/vehicles/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import (
     JSON,
+    Boolean,
     Column,
     DateTime,
     Integer,
@@ -14,12 +15,13 @@ from src.constants import DB_NAMING_CONVENTION
 
 metadata = MetaData(naming_convention=DB_NAMING_CONVENTION)
 
-auth_user = Table(
+vehicles = Table(
     "vehicles",
     metadata,
     Column("id", Uuid, primary_key=True),
     Column("name", String, nullable=False),
     Column("manufacturing_year", Integer, nullable=False),
+    Column("is_driveable", Boolean, nullable=True),
     Column("body", JSON, nullable=True),
     Column("created_at", DateTime, server_default=func.now(), nullable=False),
     Column("updated_at", DateTime, onupdate=func.now()),
