330d701831657ece2c3fce3225645a05b394d8cd
Markus Iorio <mbalzert@gmail.com>
2024-04-24 13:53:33 +0200

    remove old api

--- src/api/routes/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from src.api.routes import service_health, vehicle
-
-router = APIRouter()
-router.include_router(service_health.router, tags=["service health"], prefix="/service")
-router.include_router(vehicle.router, tags=["vehicles"], prefix="/vehicles")
