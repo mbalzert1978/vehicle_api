8e8821a9ed1e3023b83c5d8fa4b0566c82439ee0
Markus Iorio <mbalzert@gmail.com>
2024-04-25 19:26:13 +0200

    no dependecy inject from fastapi so we can handle error ourself

--- src/health/router.py
+++ src/health/router.py
@@ -1,13 +1,11 @@
 """FastAPI service module."""
 
 from enum import Enum
-from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 from sqlalchemy.exc import NoResultFound
-from sqlalchemy.ext.asyncio import AsyncConnection
 
-from src.database import get_connection
+from src.database import engine
 from src.health.constants import Tag
 from src.health.schemas import DatabaseStatus
 from src.health.services import get_database_status
@@ -18,13 +16,12 @@ router = APIRouter(prefix=Tag.HEALTH, tags=tags)
 
 
 @router.get("/", response_model=DatabaseStatus)
-async def database_status(
-    connection: Annotated[AsyncConnection, Depends(get_connection)],
-) -> DatabaseStatus:
+async def database_status() -> DatabaseStatus:
     """Check the database status."""
     try:
-        await get_database_status(connection)
-    except NoResultFound:
+        async with engine.connect() as connection:
+            await get_database_status(connection)
+    except (OSError, NoResultFound):
         return DatabaseStatus(status="Error")
     else:
         return DatabaseStatus(status="OK")
