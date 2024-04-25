5a92d95103fcae1013e8f3a8e2777165ed41fe40
Markus Iorio <mbalzert@gmail.com>
2024-04-25 19:39:34 +0200

    silence mypy for record

--- src/core/logging.py
+++ src/core/logging.py
@@ -2,11 +2,11 @@ import logging
 import sys
 
 from asgi_correlation_id import correlation_id
-from loguru import Record, logger
+from loguru import logger
 
 
-def _correlation_id_filter(record: Record) -> bool:
-    record |= {"correlation_id": correlation_id.get()}  # type: ignore[misc]
+def _correlation_id_filter(record) -> bool:
+    record |= {"correlation_id": correlation_id.get()}
     return True
 
 
