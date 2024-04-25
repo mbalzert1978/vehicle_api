51b594f24fbd05618a86fbe426d41c47e8b26818
Markus Iorio <mbalzert@gmail.com>
2024-04-25 19:38:41 +0200

    fix import error for Record

--- src/core/logging.py
+++ src/core/logging.py
@@ -1,12 +1,13 @@
 import logging
 import sys
+from typing import Any
 
 from asgi_correlation_id import correlation_id
-from loguru import Record, logger
+from loguru import logger
 
 
-def _correlation_id_filter(record: Record) -> bool:
-    record |= {"correlation_id": correlation_id.get()}  # type: ignore[misc]
+def _correlation_id_filter(record: dict[str, Any]) -> bool:
+    record |= {"correlation_id": correlation_id.get()}
     return True
 
 
@@ -15,12 +16,12 @@ def configure_logging() -> None:
     logging.getLogger("uvicorn.error").disabled = True
     logging.getLogger("uvicorn.access").disabled = True
     fmt = "[{time}] [{correlation_id}] [{level}] - {name}:{function}:{line} :: {message}"
-    logger.add(sys.stdout, format=fmt, level="INFO", filter=_correlation_id_filter)
+    logger.add(sys.stdout, format=fmt, level="INFO", filter=_correlation_id_filter)  # type:ignore[arg-type]
     logger.add(
         "logs/app.log",
         serialize=True,
         level="INFO",
-        filter=_correlation_id_filter,
+        filter=_correlation_id_filter,  # type:ignore[arg-type]
         rotation="30 MB",
         retention="7 days",
     )
