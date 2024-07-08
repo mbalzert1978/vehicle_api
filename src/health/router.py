1ab10ea145c9214e3602795aa9b30df871f025c2
Markus Iorio <mbalzert@gmail.com>
2024-07-08 10:38:59 +0200

    inject connection into health

--- src/health/router.py
+++ src/health/router.py
@@ -1,11 +1,13 @@
 """FastAPI service module."""
 
 from enum import Enum
+from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from sqlalchemy.exc import NoResultFound
+from sqlalchemy.ext.asyncio import AsyncConnection
 
-from src.database import engine
+from src.database import get_connection
 from src.health.constants import Tag
 from src.health.schemas import DatabaseStatus
 from src.health.services import get_database_status
@@ -16,11 +18,12 @@ router = APIRouter(prefix=Tag.HEALTH, tags=tags)
 
 
 @router.get("/", response_model=DatabaseStatus)
-async def database_status() -> DatabaseStatus:
+async def database_status(
+    connection: Annotated[AsyncConnection, Depends(get_connection)],
+) -> DatabaseStatus:
     """Check the database status."""
     try:
-        async with engine.connect() as connection:
-            await get_database_status(connection)
+        await get_database_status(connection)
     except (OSError, NoResultFound):
         return DatabaseStatus(status="ERROR")
     else:
