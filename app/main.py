5d145fa2ced0ccea018b2ea2b62f7b4e4e3baa36
Markus Iorio <mbalzert@gmail.com>
2024-12-19 13:52:43 +0100

    refactor: cleanup main

--- app/main.py
+++ app/main.py
@@ -2,29 +2,20 @@
 
 import logging
 
-import asgi_correlation_id as cid
 import fastapi
-import starlette.middleware.base as sbase
-import starlette.middleware.cors as scors
 import uuid_utils as uuid
+from asgi_correlation_id import CorrelationIdMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware.cors import CORSMiddleware
 
 from app import vehicles
 from app.config import get_settings
 from app.logging import configure_logging
 from app.middlewares.log import logging_middleware
 from app.middlewares.time import add_process_time_header
+from app.utils.utils import is_valid_uuid7
 
-logger = logging.getLogger(__name__)
-
-
-def _is_valid_uuid7(uuid_: str) -> bool:
-    """
-    Check whether a string is a valid v4 uuid.
-    """
-    try:
-        return bool(uuid.UUID(uuid_, version=7))
-    except ValueError:
-        return False
+CORRELATION_HEADER = "X-Correlation-ID"
 
 
 def get_application() -> fastapi.FastAPI:
@@ -34,24 +25,21 @@ def get_application() -> fastapi.FastAPI:
     application = fastapi.FastAPI(**settings.fastapi_kwargs)
 
     application.add_middleware(
-        scors.CORSMiddleware,
+        CORSMiddleware,
         allow_origins=settings.CORS_ORIGINS,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=settings.CORS_HEADERS,
     )
 
+    application.add_middleware(BaseHTTPMiddleware, dispatch=add_process_time_header)
+    application.add_middleware(BaseHTTPMiddleware, dispatch=logging_middleware)
     application.add_middleware(
-        sbase.BaseHTTPMiddleware, dispatch=add_process_time_header
-    )
-    application.add_middleware(sbase.BaseHTTPMiddleware, dispatch=logging_middleware)
-
-    application.add_middleware(
-        cid.CorrelationIdMiddleware,
-        header_name="X-Correlation-ID",
+        CorrelationIdMiddleware,
+        header_name=CORRELATION_HEADER,
         update_request_header=True,
         generator=lambda: uuid.uuid7().hex,
-        validator=_is_valid_uuid7,
+        validator=is_valid_uuid7,
         transformer=lambda a: a,
     )
 
--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,6 +1,18 @@
 import datetime
 
+import uuid_utils as uuid
+
 
 def utc_now() -> datetime.datetime:
     """Returns a utc timezone aware datetime object."""
     return datetime.datetime.now(tz=datetime.timezone.utc)
+
+
+def is_valid_uuid7(value: str) -> bool:
+    """
+    Check whether a string is a valid v4 uuid.
+    """
+    try:
+        return bool(uuid.UUID(value, version=7))
+    except ValueError:
+        return False
